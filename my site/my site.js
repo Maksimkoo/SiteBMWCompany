3833c65dee65db89cac60f851aa742573a2949ef
Maksimkoo <55229728+Maksimkoo@users.noreply.github.com>
2020-12-30 21:35:00 +0200

    site

---
+++ my site/my site.js
@@ -0,0 +1,11 @@
+menu.onclick = function myFunction(){
+  var x = document.getElementById('myTopnav');
+
+  if (x.className === "topnav") {
+  x.className += " responsive";
+ } else {
+  x.className = "topnav";
+ }
+}
+
+
